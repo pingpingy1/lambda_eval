1fa8fe4c9d925e66adb9faf261cdb073b21bab53
Heewon Lee <pingpingy@kaist.ac.kr>
2026-09-16 20:20:42 +0900

    [Feat] better description of the project

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,15 @@ open Lambda_eval.Encode
 open Lambda_eval.Eval
 open Lambda_eval.Utils
 
+let print_description : unit =
+  print_endline "This project takes the abstract syntax tree (AST) of a lambda term,";
+  print_endline "represents it as a separate lambda term (so-called Morgensen-Scott";
+  print_endline "encoding), and evaluates into the term it encodes.";
+  print_endline "The evaluator is given by:";
+  print_endline (term_to_string e_eval);
+  print_endline ""
+
+
 let test_term (t : term) : unit =
   let repr : term = encode t in
   let eval_repr : term = normalize 1000 (App (e_eval, repr)) in
@@ -10,5 +19,6 @@ let test_term (t : term) : unit =
   print_endline ("Evaluated:      " ^ term_to_string eval_repr);
   print_endline ""
 
+let _ = print_description
 let _ = test_term (Lam (Var 0))
 let _ = test_term (App (Lam (Var 0), Lam (Var 0)))
